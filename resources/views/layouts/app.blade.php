c93adc688776a00c150ee8e4aaf12d51496c9413
Sudipta Ankanmoy <sudiptaa30@gmail.com>
2025-04-23 12:22:14 -0700

    add custom css file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link href="css/custom.css" rel="stylesheet" media="all">
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
