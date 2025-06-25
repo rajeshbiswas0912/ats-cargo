70d08bab050d121d387f4368a211e45f5d111b2b
Rajesh Biswas <rajesh91.creativetech@gmail.com>
2025-06-25 22:21:08 +0530

    update labels

--- resources/views/orders/edit.blade.php
+++ resources/views/orders/edit.blade.php
@@ -32,24 +32,29 @@
                 <div class="row">
                   <div class="col-lg-3">
                     <div class="form-group">
+                      <label class="form-label" for="" style="color: black; font-size: 15px;">Name</label>
                       <input type="text" class="form-control" id="p_name" name="p_name"
                         value="{{ $order->pickup_name }}" placeholder="Name">
                     </div>
                   </div>
                   <div class="col-lg-3">
                     <div class="form-group">
+                      <label class="form-label" for="" style="color: black; font-size: 15px;">Pincode</label>
                       <input type="number" class="form-control" id="p_pincode" name="p_pincode"
                         value="{{ $order->pickup_pincode }}" placeholder="Pincode">
                     </div>
                   </div>
                   <div class="col-lg-3">
                     <div class="form-group">
+                      <label class="form-label" for="" style="color: black; font-size: 15px;">Mobile
+                        Number</label>
                       <input type="number" class="form-control" id="p_mobile" name="p_mobile"
                         value="{{ $order->pickup_mobile }}" placeholder="Mobile Number">
                     </div>
                   </div>
                   <div class="col-lg-12">
                     <div class="form-group">
+                      <label class="form-label" for="" style="color: black; font-size: 15px;">Address</label>
                       <input type="text" class="form-control" id="p_address" name="p_address"
                         value="{{ $order->pickup_address }}" placeholder="Address">
                     </div>
@@ -70,24 +75,29 @@
                 <div class="row">
                   <div class="col-lg-3">
                     <div class="form-group">
+                      <label class="form-label" for="" style="color: black; font-size: 15px;">Name</label>
                       <input type="text" class="form-control" id="d_name" name="d_name"
                         value="{{ $order->delivery_name }}" placeholder="Name">
                     </div>
                   </div>
                   <div class="col-lg-3">
                     <div class="form-group">
+                      <label class="form-label" for="" style="color: black; font-size: 15px;">Pincode</label>
                       <input type="number" class="form-control" id="d_pincode" name="d_pincode"
                         value="{{ $order->delivery_pincode }}" placeholder="Pincode">
                     </div>
                   </div>
                   <div class="col-lg-3">
                     <div class="form-group">
+                      <label class="form-label" for="" style="color: black; font-size: 15px;">Mobile
+                        Number</label>
                       <input type="number" class="form-control" id="d_mobile" name="d_mobile"
                         value="{{ $order->delivery_mobile }}" placeholder="Mobile Number">
                     </div>
                   </div>
                   <div class="col-lg-12">
                     <div class="form-group">
+                      <label class="form-label" for="" style="color: black; font-size: 15px;">Address</label>
                       <input type="text" class="form-control" id="d_address" name="d_address"
                         value="{{ $order->delivery_address }}" placeholder="Address">
                     </div>
@@ -114,36 +124,49 @@
                       <div class="row {{ $key > 1 ? 'other_packages_' . $key : null }}">
                         <div class="col-lg-6">
                           <div class="form-group">
+                            <label class="form-label" for="" style="color: black; font-size: 15px;">Material
+                              type</label>
                             <input type="text" class="form-control" id="material_type" name="material_type[]"
                               placeholder="Material type" value="{{ $item->material_type }}">
                           </div>
                         </div>
                         <div class="col-lg-3">
                           <div class="form-group">
+                            <label class="form-label" for="" style="color: black; font-size: 15px;">No of
+                              boxes</label>
                             <input type="number" class="form-control" id="no_of_boxes" name="no_of_boxes[]"
                               placeholder="No of boxes" value="{{ $item->no_of_box }}">
                           </div>
                         </div>
                         <div class="col-lg-3">
                           <div class="form-group">
+                            <label class="form-label" for=""
+                              style="color: black; font-size: 15px;">Weight</label>
                             <input type="number" class="form-control" id="weight" name="weight[]"
                               placeholder="Weight" value="{{ $item->weight }}">
                           </div>
                         </div>
                         <div class="col-lg-2">
                           <div class="form-group">
+                            <label class="form-label" for="" style="color: black; font-size: 15px;">Height
+                              (cm)
+                            </label>
                             <input type="number" class="form-control" id="height" name="height[]"
                               placeholder="Height (cm)" value="{{ $item->height }}">
                           </div>
                         </div>
                         <div class="col-lg-2">
                           <div class="form-group">
+                            <label class="form-label" for="" style="color: black; font-size: 15px;">Length
+                              (cm)</label>
                             <input type="number" class="form-control" id="length" name="length[]"
                               placeholder="Length (cm)" value="{{ $item->length }}">
                           </div>
                         </div>
                         <div class="col-lg-2">
                           <div class="form-group">
+                            <label class="form-label" for="" style="color: black; font-size: 15px;">Width
+                              (cm)</label>
                             <input type="number" class="form-control" id="width" name="width[]"
                               placeholder="Width (cm)" value="{{ $item->width }}">
                           </div>
@@ -184,12 +207,15 @@
                 <div class="row">
                   <div class="col-lg-3">
                     <div class="form-group">
+                      <label class="form-label" for="" style="color: black; font-size: 15px;">Price</label>
                       <input type="number" class="form-control" id="price" name="price" placeholder="Price"
                         value="{{ $order->total_amount }}" required>
                     </div>
                   </div>
                   <div class="col-lg-3">
                     <div class="form-group">
+                      <label class="form-label" for="" style="color: black; font-size: 15px;">Payment
+                        Type</label>
                       <select name="payment_type" id="payment_type" class="form-control" required>
                         <option value="0">Please select</option>
                         <option value="prepaid" {{ $order->payment_type == 'prepaid' ? 'selected' : null }}>Prepaid
@@ -215,42 +241,53 @@
                 <div class="row">
                   <div class="col-lg-3">
                     <div class="form-group">
+                      <label class="form-label" for="" style="color: black; font-size: 15px;">Shipping
+                        Price</label>
                       <input type="number" class="form-control" id="shipping_charge" name="shipping_charge"
                         placeholder="Shipping Price" value="{{ $order->shipping_charge }}">
                     </div>
                   </div>
                   <div class="col-lg-3">
                     <div class="form-group">
+                      <label class="form-label" for="" style="color: black; font-size: 15px;">Pickup
+                        Charge</label>
                       <input type="number" class="form-control" id="pickup_charge" name="pickup_charge"
                         placeholder="Pickup Charge" value="{{ $order->pickup_charge }}">
                     </div>
                   </div>
                   <div class="col-lg-3">
                     <div class="form-group">
+                      <label class="form-label" for="" style="color: black; font-size: 15px;">Hamali</label>
                       <input type="number" class="form-control" id="hamali" name="hamali" placeholder="Hamali"
                         value="{{ $order->hamali }}">
                     </div>
                   </div>
                   <div class="col-lg-3">
                     <div class="form-group">
+                      <label class="form-label" for="" style="color: black; font-size: 15px;">S/C</label>
                       <input type="number" class="form-control" id="sc_cost" name="sc_cost" placeholder="S/C"
                         value="{{ $order->sc_cost }}">
                     </div>
                   </div>
                   <div class="col-lg-3">
                     <div class="form-group">
+                      <label class="form-label" for="" style="color: black; font-size: 15px;">ST.
+                        Charge</label>
                       <input type="number" class="form-control" id="st_charge" name="st_charge"
                         placeholder="ST. Charge" value="{{ $order->st_charge }}">
                     </div>
                   </div>
                   <div class="col-lg-3">
                     <div class="form-group">
+                      <label class="form-label" for="" style="color: black; font-size: 15px;">Delivery
+                        Charge</label>
                       <input type="number" class="form-control" id="delivery_charge" name="delivery_charge"
                         placeholder="Delivery Charge" value="{{ $order->delivery_charge }}">
                     </div>
                   </div>
                   <div class="col-lg-3">
                     <div class="form-group">
+                      <label class="form-label" for="" style="color: black; font-size: 15px;">IGST</label>
                       <input type="number" class="form-control" id="igst" name="igst" placeholder="IGST"
                         value="{{ $order->igst }}">
                     </div>
